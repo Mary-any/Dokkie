67693473cbe9cb6035551e34a647576d3ee82293
Mary Anyanwu <maria.anyanwu@hva.nl>
2023-09-29 12:25:53 +0200

    dokkie/login

---
+++ src/dokkie.js
@@ -0,0 +1,52 @@
+// Select The Elments
+let toggle_btn;
+let big_wrapper;
+
+function declare(){
+    toggle_btn = document.querySelector(".toggle-btn");
+    big_wrapper = document.querySelector(".big-wrapper");
+}
+
+const main = document.querySelector("main");
+
+declare();
+let dark = false;
+
+function toggleAnimation(){
+    // Clone the wrapper
+    dark = !dark;
+    let clone= big_wrapper.cloneNode(true);
+    if (dark) {
+        clone.classList.remove("light");
+        clone.classList.add("dark");
+    } else{
+        clone.classList.remove("dark");
+        clone.classList.add("light");
+    }
+    clone.classList.add("copy");
+    main.appendChild(clone);
+
+    document.body.classList.add("stop-scrolling");
+
+    clone.addEventListener("animationend",() => {
+        document.body.classList.remove("stop-scrolling");
+        big_wrapper.remove();
+        clone.classList.remove("copy");
+        //Reset Varibles
+        declare();
+        events();
+    }); 
+    
+    // const menu_btn= document.querySelector(".hamburger");
+
+    // menu_btn.addEventListener("click", function (){
+    //     menu_btn.classList.hamburger("is-active");
+    // });
+}
+
+
+function events(){
+
+}
+
+events();
